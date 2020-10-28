3528e9ea1cd9d9a5cd9a3d84d72f7c57a24a15bf
Ivan Aguirre <you@example.com>
2020-10-27 22:55:32 -0700

    Updates

---
+++ precache-manifest.0f06120e92d35c2f57967b099e68d178.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28156a8c05b76404c657ed4b3798e318",
+    "url": "/Practica1/index.html"
+  },
+  {
+    "revision": "dae8e11bd05f9b31feb0",
+    "url": "/Practica1/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "17ece542ced2767f7243",
+    "url": "/Practica1/static/css/main.c22f9c82.chunk.css"
+  },
+  {
+    "revision": "dae8e11bd05f9b31feb0",
+    "url": "/Practica1/static/js/2.48adb87e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Practica1/static/js/2.48adb87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ece542ced2767f7243",
+    "url": "/Practica1/static/js/main.40339656.chunk.js"
+  },
+  {
+    "revision": "043686b53e7e3d1d6e26",
+    "url": "/Practica1/static/js/runtime-main.d386a4f6.js"
+  },
+  {
+    "revision": "96aff77c5833d91511a87f051dc63231",
+    "url": "/Practica1/static/media/LogoTecNM.96aff77c.jpg"
+  }
+]);
