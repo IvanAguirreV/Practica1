f84b8a2cd36349a89c014525e1a75de066a8d293
Ivan Aguirre <you@example.com>
2020-10-27 20:48:04 -0700

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Componentes/Header';
+import Body from './Componentes/Body';
+import Footer from './Componentes/Footer';
+import Fecha from './Componentes/Fecha';
+import Logo from './Componentes/Logo';
 
-function App() {
+class App extends Component{
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Logo/>
+      <Fecha/>
+      <Body/>
+      <Footer copyr = {<p>Copyright &copy; Todos los derechos reservados.</p>} />
     </div>
-  );
+  )};
 }
 
 export default App;
---
+++ src/Componentes/Header.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Header.css';
+
+class Header extends Component{
+    render(){
+        return(
+            <div className = "Header">
+                <p>Desarrollo de aplicaciones para dispositivos moviles</p>
+            </div>
+        )
+    }
+}
+
+export default Header;
